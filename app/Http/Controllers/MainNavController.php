db6d55bbf851de4da0249708c86309bc41beed5b
Muhammad Junaid Rehman Siddiqui <jbhai8058@gmail.com>
2023-06-01 14:28:36 +0500

    changes

--- app/Http/Controllers/MainNavController.php
+++ app/Http/Controllers/MainNavController.php
@@ -2,15 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
+use App\Models\Employee;
 use App\Models\MainnavModule;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class MainNavController extends Controller
 {
-    public function show($mainnavId)
-    {
-        $mainnav = MainnavModule::with('submainnavModule')->find($mainnavId);
+    // public function show($mainnavId)
+    // {
+    //     $mainnav = MainnavModule::with('submainnavModule')->find($mainnavId);
 
-        return response()->json($mainnav);
+    //     return response()->json($mainnav);
+    // }
+
+
+    public function getData()
+    {
+        // Get the employee's department
+        $employeeId = Employee::all('id'); // Replace with the desired employee_id
+        $department = Department::where('employee_id', $employeeId)->first();
+
+        if (!$department) {
+            return response()->json(['message' => 'Department not found'], 404);
+        }
+
+        // Get the projects for the department
+        $projects = Project::where('department_id', $department->id)
+            ->where('employee_id', $employeeId)
+            ->get();
+
+        return response()->json(['department' => $department, 'projects' => $projects]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 
-Route::get('mainnav/{mainnav}', [MainNavController::class, 'show']);
+Route::get('data', [MainNavController::class, 'getData']);
 
 Route::get('asidebar/{mainnav}', [AsidebarController::class, 'show']);
